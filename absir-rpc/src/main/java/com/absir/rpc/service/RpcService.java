8d48777b1e92183ec3f7f4e0e172cdc200828f3e
absir <absir@qq.com>
2016-08-19 16:55:27 +0800

    优化JTask、JPlan 计划任务执行

---
+++ absir-rpc/src/main/java/com/absir/rpc/service/RpcService.java
@@ -0,0 +1,13 @@
+package com.absir.rpc.service;
+
+import com.absir.bean.inject.value.Inject;
+
+/**
+ * Created by absir on 16/8/19.
+ */
+@Inject
+public abstract class RpcService {
+
+    //public
+
+}
