a8ef742447ed50fa53c21b44b651051b9dc9a0a3
absir <absir@qq.com>
2018-03-12 16:43:08 +0800

    优化 linktpk 命令

--- achieve-site/linktpl.sh
+++ achieve-site/linktpl.sh
@@ -14,7 +14,14 @@ echo "$currentDir=>$toDir"
 fromPath="$currentDir/src/main/webapp"
 toPath="$toDir/src/main/webapp"
 
+if [ -d "$toPath/static" ]; then
+	echo "$toPath/static dir exist"
+	exit 0
+fi
+
 ln -s "$fromPath/static" "$toPath/static"
 ln -s "$fromPath/lib" "$toPath/lib"
 ln -s "$fromPath/WEB-INF/developer" "$toPath/WEB-INF/developer"
 ln -s "$fromPath/WEB-INF/tpl" "$toPath/WEB-INF/tpl"
+
+mkdir "$toPath/../webapp0"
